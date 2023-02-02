6ec63e528874f941f7a06bec58298e1ef6243611
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-02-02 13:16:18 +0530

    Script Location Declaration

--- frontend.sh
+++ frontend.sh
@@ -8,25 +8,22 @@ print_head "Remove Nginx Old Content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
 status_check
 
-
-print_head "Download Frontend Content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-status_check
-
 cd /usr/share/nginx/html &>>${LOG}
 
 print_head "Extract Frontend Content"
 unzip /tmp/frontend.zip &>>${LOG}
 status_check
 
-print_head "Copy RoboShop Nginx Config File"
+print_head "Copy Roboshop Nginx Config File"
+# shellcheck disable=SC2154
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 status_check
 
-print_head "Enable Nginx"
+print_head "Enable nginx"
 systemctl enable nginx &>>${LOG}
 status_check
 
 print_head "Start Nginx"
 systemctl restart nginx &>>${LOG}
 status_check
+

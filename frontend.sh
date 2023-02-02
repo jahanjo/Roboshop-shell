895e0fcac71abbf9773adf811486b24f608a4a0a
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-02-02 19:41:03 +0530

    Frontend

--- frontend.sh
+++ frontend.sh
@@ -8,6 +8,7 @@ print_head "Remove Nginx Old Content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
 status_check
 
+
 print_head "Download Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 status_check
@@ -19,14 +20,13 @@ unzip /tmp/frontend.zip &>>${LOG}
 status_check
 
 print_head "Copy RoboShop Nginx Config File"
-cp  ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 status_check
 
-print_head "Enable nginx"
+print_head "Enable Nginx"
 systemctl enable nginx &>>${LOG}
 status_check
 
 print_head "Start Nginx"
 systemctl restart nginx &>>${LOG}
 status_check
-

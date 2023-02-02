084e6ac0220e288513d8dc50ff7fdb2dca56f588
Jahangeer <jahangeerkallagunta9@gmail.com>
2023-02-02 12:37:56 +0530

    Script Location Declaration

--- common.sh
+++ common.sh
@@ -1,6 +1,17 @@
 script_location=$(pwd)
 
-#LOG=/tmp/roboshop.log
+LOG=/tmp/roboshop.log
+
+status_check(){
+  if [ $? -eq 0 ]
+  then
+    echo -e "\e[1;31mSUCCESS\e[0m"
+  else
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo "Refer Log file for more information, LOG - ${LOG}"
+    exit 1
+  fi
+}
 
 
 
